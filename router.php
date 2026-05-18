0fe1b939ea51592a2f671b559567c32bc2453b04
mansimbisoa <mansimbisoa@MacBook-Air-de-mansimbisoa.local>
2026-05-18 22:09:16 +0300

    fix router railway

--- router.php
+++ router.php
@@ -1,15 +1,11 @@
 <?php
 
-if (php_sapi_name() === 'cli-server') {
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$file = __DIR__ . $path;
 
-    $fullPath = __DIR__ . $path;
-
-    if ($path !== '/' && file_exists($fullPath)) {
-
-        return false;
-    }
+if ($path !== "/" && file_exists($file)) {
+    return false;
 }
 
-require_once __DIR__ . '/index.php';
+include __DIR__ . "/index.php";
